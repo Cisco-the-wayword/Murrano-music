ccc1fe2711805c57ab44dc9644378431980e939f
Cisco <ciscophrenzy@gmail.com>
2023-08-17 03:24:31 -0600

    Modify app test and install stylelintrc

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import React from 'react';
+import { render, screen, test } from '@testing-library/react';
+import { expect } from '@jest/globals';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
